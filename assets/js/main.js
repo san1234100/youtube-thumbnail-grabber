861ee5ca5df4e91b7c1461d8b87835dd8984ed10
CHANDRU U <sanjeevchandru1234@gmail.com>
2024-02-27 13:34:29 +0530

    feat: display  full screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@ function getThumbnail() {
     if (videoId) {
       let thumbnailUrl = 'https://img.youtube.com/vi/' + videoId + '/maxresdefault.jpg';
       console.log(thumbnailUrl);
-      youtubeThumbnailEl.innerHTML = '<img src="' + thumbnailUrl + '">';
+      youtubeThumbnailEl.innerHTML = '<img class="w-[100%]" src="' + thumbnailUrl + '">';
     } else {
       alert('Invalid YouTube Link');
     }
@@ -24,5 +24,37 @@ function getThumbnail() {
       return match[2];
     } else {
       return null;
+    }
+  }
+
+  youtubeThumbnailEl.addEventListener('click', () => {
+    if (document.youtubeThumbnailEl) {
+      exitFullscreen();
+    } else {
+      enterFullscreen(youtubeThumbnailEl);
+    }
+  });
+  
+  function enterFullscreen(element) {
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) { /* Firefox */
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+      element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) { /* IE/Edge */
+      element.msRequestFullscreen();
+    }
+  }
+  
+  function exitFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+      document.msExitFullscreen();
     }
   }
